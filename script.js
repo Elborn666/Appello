6b66f4b9db5cd90a1612ae518adaa3cc33ddc0c9
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-03-30 00:46:59 +0300

    удаление лишнего

--- script.js
+++
@@ -1,9 +0,0 @@
-const headers = document.querySelectorAll("[data-name='spoiler-title']");
-
-headers.forEach(function (item) {
-  item.addEventListener("click", headerClick);
-});
-
-function headerClick() {
-  this.nextElementSibling.classList.toggle("spoiler-body");
-}
